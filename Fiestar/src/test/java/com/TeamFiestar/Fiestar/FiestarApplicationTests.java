fbfe713a6592f439af61ca4ab4b02e31f6e6a962
kkm990708 <kkm990708@gmail.com>
2023-11-24 14:33:23 +0900

    초기 설정

---
+++ Fiestar/src/test/java/com/TeamFiestar/Fiestar/FiestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TeamFiestar.Fiestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
