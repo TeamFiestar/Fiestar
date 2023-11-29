51ee172aaa19e082b54afe969a4ac749e6c55cc5
kkm990708 <kkm990708@gmail.com>
2023-11-29 17:49:25 +0900

    라이브 스트리밍 백엔드 완성, 미디어 리스트, 미디어, 공지사항, 공지사항 리스트, 공지사항 디테일 프론트 완성

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/live/controller/LiveController.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/live/controller/LiveController.java
@@ -2,6 +2,8 @@ package com.TeamFiestar.Fiestar.live.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,20 +15,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
+@RequestMapping("media")
 public class LiveController {
 	
-	// 토큰 생성
-    @GetMapping("/generateToken")
-    @ResponseBody
-    public String generateAgoraToken(
-            @RequestParam(value="flag", required = false, defaultValue = "1") int flag
-    ) {
+	@GetMapping("live")
+	public String moveLive() {
+		return "media/media";
+	}
+	
+	@ResponseBody
+	@GetMapping("generateToken")
+	public String generateAgoraToken(@RequestParam("roleFlag") int roleFlag,
+			@RequestParam(name = "channelName", required = false, defaultValue = "kang") String channelName
+			
+			) {
     	
     	RtcTokenBuilder2 tokenBuilder = new RtcTokenBuilder2();
-    	Role rtcRole = null;
-    	if (flag == 1) {
-    		rtcRole = Role.ROLE_PUBLISHER;
-		}else {
+    	Role rtcRole = Role.ROLE_PUBLISHER;
+    	if (roleFlag != 1) {
 			rtcRole = Role.ROLE_SUBSCRIBER;
 		}
     	
@@ -34,9 +40,7 @@ public class LiveController {
         String appId = "8db96ea10154462a8fcb52cee3d45ccb";
         String appCertificate = "d9b7838da2cb4bd0b3d9a9aa1c82eba6";
         int expirationTimeInSeconds = 36000;
-        int uid = 2;
-        String channelName = "kang2";
-        
+        int uid = 0;
 
         String token = tokenBuilder.buildTokenWithUid(
                 appId,
