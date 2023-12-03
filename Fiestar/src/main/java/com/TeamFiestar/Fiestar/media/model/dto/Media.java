150b4ba008eb96ce3a42fb5c5820b55143a4e642
kkm990708 <kkm990708@gmail.com>
2023-12-04 03:10:51 +0900

    미디어 dto service mapper 추가,  기본적인 코드 작성

---
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/media/model/dto/Media.java
@@ -0,0 +1,13 @@
+package com.TeamFiestar.Fiestar.media.model.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Media {
+
+	private String mediaTitle;
+	private String mediaUrl;
+	
+}
