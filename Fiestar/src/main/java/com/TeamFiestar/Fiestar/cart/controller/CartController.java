63bd41d7e7c1914dd9ece808b41947c200ba2485
Leeee <anthem109@naver.com>
2023-12-04 18:01:31 +0900

    작업 커밋

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/cart/controller/CartController.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/cart/controller/CartController.java
@@ -1,9 +1,12 @@
 package com.TeamFiestar.Fiestar.cart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.TeamFiestar.Fiestar.cart.model.dto.Cart;
 
 
 
@@ -12,11 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CartController {
 	
 	@GetMapping("cart")
-	public String cart() {
+	public String cart(Model model, Cart cart) {
 		
+		model.addAttribute("cart", cart);
 		
 		return "cart/cart";
 	}
+	
 	
 	@GetMapping("checkoutResult")
 	public String checkoutResult() {
@@ -24,7 +29,7 @@ public class CartController {
 		return "cart/checkout-result";
 	}
 	
-	@GetMapping("checkout")
+	@PostMapping("checkout")
 	public String checkout() {
 		return "cart/checkout";
 	}
---
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/cart/model/dto/Cart.java
@@ -0,0 +1,11 @@
+package com.TeamFiestar.Fiestar.cart.model.dto;
+
+public class Cart {
+	
+	private int product_no;
+	private String product_name;
+	private String product_img;
+	private String product_price;
+	private String product_count;
+	
+}
