ae77f72859ecc5e524bdcef3c8b48afea6fe43ef
David <yhoon_0802@hotmail.com>
2023-12-05 17:49:27 +0900

    231205 ARTISTHOMEPAGE

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/board/controller/BoardController.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/board/controller/BoardController.java
@@ -6,31 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.TeamFiestar.Fiestar.board.model.service.BoardService;
 
+import lombok.RequiredArgsConstructor;
+
 @Controller
-@RequestMapping("{artistGroupTitle}")
 @SessionAttributes({"loginMember"})
+@RequiredArgsConstructor
 public class BoardController {
 	
-	@Autowired
-	private BoardService service;
 	
-	@GetMapping("feed")
-	public String selectBoard(Model model, @RequestParam Map<String, Object> paramMap) {
+	private final BoardService service;
+	
+	@GetMapping("{artistGroupTitle}/feed")
+	public String selectBoard(Model model, @RequestParam Map<String, Object> paramMap, 
+			@PathVariable("artistGroupTitle" ) String artistGroupTitle) {
 		{
 			
 			Map<String, Object> map = service.selectBoard(paramMap);
 			
 			model.addAttribute("map", map);
+			model.addAttribute("artistGroupTitle", artistGroupTitle);
 			
 		}
 		
-		return "{artistGroupTitle}/feed";
+		return "artistHomepage/feed";
 	}
 
+	
+	@GetMapping("{artistGroupTitle}/artist")
+	public String artist() {
+		return "artistHomepage/artist";
+	}
 }
