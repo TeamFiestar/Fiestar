47550027e6e085d3c1220bc3df810f88fb0de6ee
Leeee <anthem109@naver.com>
2023-12-05 17:39:23 +0900

    1205 컨트롤러 작업

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/cart/model/dto/Cart.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/cart/model/dto/Cart.java
@@ -1,11 +1,22 @@
 package com.TeamFiestar.Fiestar.cart.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Cart {
 	
-	private int product_no;
-	private String product_name;
-	private String product_img;
-	private String product_price;
-	private String product_count;
+	private String memberNo;
+	private String productName;
+	private String productImg;
+	private String productPrice;
+	private String productCount;
+	private int productNo;
+	private String productOptionName;
+	private int cartNo;
+	
+	
 	
 }
