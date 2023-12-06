7865dc7f85779fe1b72898e22f21b4cf30062850
Leeee <anthem109@naver.com>
2023-12-06 09:01:08 +0900

    no message

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/mypage/service/MyPageService.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/mypage/service/MyPageService.java
@@ -1,17 +1,17 @@
-package com.TeamFiestar.Fiestar.mypage.service;
-
-import java.io.IOException;
-import java.util.Map;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import com.TeamFiestar.Fiestar.member.model.DTO.Member;
-
-public interface MyPageService {
-
-	// 프로필 이미지 바꾸기
-	int profile(Member loginMember, MultipartFile memberProfile) throws IllegalStateException, IOException;
-
-
-
-}
+//package com.TeamFiestar.Fiestar.mypage.service;
+//
+//import java.io.IOException;
+//import java.util.Map;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import com.TeamFiestar.Fiestar.member.model.DTO.Member;
+//
+//public interface MyPageService {
+//
+//	// 프로필 이미지 바꾸기
+//	int profile(Member loginMember, MultipartFile memberProfile) throws IllegalStateException, IOException;
+//
+//
+//
+//}
--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/shop/controller/ShopController.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/shop/controller/ShopController.java
@@ -1,101 +1,100 @@
-package com.TeamFiestar.Fiestar.shop.controller;
-
-<<<<<<< HEAD
-import org.springframework.stereotype.Controller; 
-=======
-
-import java.util.Map;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
->>>>>>> origin/main
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import com.TeamFiestar.Fiestar.shop.model.service.ShopService;
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequestMapping("shop")
-@RequiredArgsConstructor
-public class ShopController {
-	
-	private final ShopService service;
-
-	
-	@GetMapping("home")
-	public String shopMain(Model model) {
-		
-		Map<String, Object> map = service.shopMain();
-		model.addAttribute("map",map);
-	
-		int shopCount = service.shopMainCount();
-		model.addAttribute("shopCount", shopCount);
-		
-		
-		return "shop/home";
-	}	
-	
-	
-
-	
-	
-
-	
-	
-	
-	@GetMapping("noticeDetail")
-	public String noticeDetail() {
-		return "shop/noticeDetail";
-	}
-	
-	
-	
-	
-	
-	
-	@GetMapping("shopNotice")
-	public String shopNotice() {
-		return "shop/shopNotice";
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	@GetMapping("noticeList")
-	public String noticeList() {
-		return "shop/noticeList";
-	}
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	@GetMapping("shopDetail")
-	public String shopDetail() {
-		return "shop/shopDetail";
-			
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-}
+//package com.TeamFiestar.Fiestar.shop.controller;
+//
+//
+//import org.springframework.stereotype.Controller;  
+//
+//import java.util.Map;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import com.TeamFiestar.Fiestar.shop.model.service.ShopService;
+//import lombok.RequiredArgsConstructor;
+//
+//@Controller
+//@RequestMapping("shop")
+//
+//public class ShopController {
+//	
+////	private final ShopService service;
+//
+//	
+//	@GetMapping("home")
+//	public String shopMain(Model model) {
+//		
+//		Map<String, Object> map = service.shopMain();
+//		model.addAttribute("map",map);
+//	
+//		int shopCount = service.shopMainCount();
+//		model.addAttribute("shopCount", shopCount);
+//		
+//		
+//		return "shop/home";
+//	}	
+//	
+//	
+//
+//	
+//	
+//
+//	
+//	
+//	
+//	@GetMapping("noticeDetail")
+//	public String noticeDetail() {
+//		return "shop/noticeDetail";
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	@GetMapping("shopNotice")
+//	public String shopNotice() {
+//		return "shop/shopNotice";
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	@GetMapping("noticeList")
+//	public String noticeList() {
+//		return "shop/noticeList";
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//
+//	
+//	
+//	
+//	
+//	
+//	
+//	@GetMapping("shopDetail")
+//	public String shopDetail() {
+//		return "shop/shopDetail";
+//			
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//
+//}
