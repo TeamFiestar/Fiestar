822e8ab0d37e377e1572927a60857aacb87572e0
MOMO <rhwoah7190@gmail.com>
2023-12-07 12:18:55 +0900

    관리자페이지(굿즈 등록) 프론트 수정, 관리자 백엔드

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/admin/controller/AdminController.java
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/admin/controller/AdminController.java
@@ -1,13 +1,40 @@
 package com.TeamFiestar.Fiestar.admin.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.TeamFiestar.Fiestar.admin.model.service.adminService;
+import com.TeamFiestar.Fiestar.member.model.dto.Member;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("admin")
+@RequiredArgsConstructor
 public class AdminController {
 
-	@GetMapping("/admin")
+	private final adminService service;
+	
+	@GetMapping("selectMember")
+	public String member(Model model) {
+		
+		List<Member> memberList = service.selectMember();
+		model.addAttribute("memberList",memberList);
+		return "admin/selectMember";
+	}
+	
+	
+	@GetMapping("notice")
 	public String admin() {
 		return "admin/artistNotice";
+	}
+	
+	@GetMapping("register")
+	public String register() {
+		return "admin/goods";
 	}
 }
