dd3ffdf6776dfff2b9d62c1a871de82318bbdb72
momo <rhwoah7190@gmail.com>
2023-12-12 01:57:39 +0900

    관리자 페이지 (탈퇴,복구 비동기)

--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/media/agora/media/Packable.java
+++
@@ -1,8 +0,0 @@
-package com.TeamFiestar.Fiestar.media.agora.media;
-
-/**
- * Created by Li on 10/1/2016.
- */
-public interface Packable {
-    ByteBuf marshal(ByteBuf out);
-}
--- Fiestar/src/main/java/com/TeamFiestar/Fiestar/media/agora/media/PackableEx.java
+++
@@ -1,5 +0,0 @@
-package com.TeamFiestar.Fiestar.media.agora.media;
-
-public interface PackableEx extends Packable {
-    void unmarshal(ByteBuf in);
-}
