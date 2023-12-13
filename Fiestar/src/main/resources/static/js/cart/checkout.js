170c26a3efee7b8e0759c099fbc77aba0183479d
창우 <>
2023-12-13 23:38:51 +0900

    결제 페이지

--- Fiestar/src/main/resources/static/js/cart/checkout.js
+++ Fiestar/src/main/resources/static/js/cart/checkout.js
@@ -1,51 +1,30 @@
-// 변경 버튼 클릭 -> 인풋에 채워진 텍스트 모두 지우기
+// // 변경 버튼 클릭 -> 인풋에 채워진 텍스트 모두 지우기
 
-const nameBtn = document.getElementById("name-btn");
+// const nameChangeBtn = document.getElementById("nameChangeBtn");
 
-nameBtn.addEventListener("click", e => {
+// nameChangeBtn.addEventListener("click", e => {
 
-    let nameInput = e.target.nextElementSibling;
+//     let name = e.target.nextElementSibling[0];
     
-    if(!nameInput.trim().length == 0){
-        nameInPut = ("");
-    }
+//     if(!nameInput.trim().length == 0){
+//         nameInPut = ("");
+//     }
 
+// });
+const nameChangeBtn = document.getElementById("nameChangeBtn");
+
+nameChangeBtn.addEventListener("click", e => {
+    
+    let nameInput = document.getElementById("nameInput");
+    let address1 = document.getElementById("address1");
+    let address2 = document.getElementById("address2");
+    let address3 = document.getElementById("address3");
+
+    if(nameInput.value.trim().length || address1.value || address2 || address3 !== 0){
+        nameInput.value = ("");
+        address1.value =("");
+        address2.value =("");
+        address3.value =("");
+    }
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-const memberAddress = /*[[${session.loginMember.memberAddress}]]*/ "회원주소"
-
-// 주소가 있을 경우 (A^^^B^^^c)
-if(memberAddress != null) {
-
-    // 1) 문자열을 ^^^을 기준으로 쪼개어 배열로 반환
-    // const = memberAddress.split("^^^"); // 
-
-}
-
-//     // 2) name 속성값이 "memberAddress"인 요소를 모두 얻어옴
-//     const inputs = document.querySelectorAll("[name='memberAddress']");
-    
-//     // 배열 or NodeList.forEach( () => {} )
-//     // 배열 or NodeList의 모든 요소를 순차 접근하는 향상된 for문
-//     // item : 현재 접근 중인 요소
-//     // index : 현재 접근 중인 요소의 인덱스가 담긴다
-
-//     // 3) input 요소에 arr의 값을 순서대로 대입
-//     inputs.forEach( (item, index) => {
-
-//         item.value = arr[index]; 
-
-//     } );
-// }
