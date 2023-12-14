e8becda9ae26cf2133fbcc0fec5f101232d7585b
Leeee <anthem109@naver.com>
2023-12-14 11:29:17 +0900

    결제 페이지 작업

---
+++ Fiestar/src/main/resources/static/js/cart/checkout copy.js
@@ -0,0 +1,30 @@
+// // 변경 버튼 클릭 -> 인풋에 채워진 텍스트 모두 지우기
+
+// const nameChangeBtn = document.getElementById("nameChangeBtn");
+
+// nameChangeBtn.addEventListener("click", e => {
+
+//     let name = e.target.nextElementSibling[0];
+    
+//     if(!nameInput.trim().length == 0){
+//         nameInPut = ("");
+//     }
+
+// });
+const nameChangeBtn = document.getElementById("nameChangeBtn");
+
+nameChangeBtn.addEventListener("click", e => {
+    
+    let nameInput = document.getElementById("nameInput");
+    let address1 = document.getElementById("address1");
+    let address2 = document.getElementById("address2");
+    let address3 = document.getElementById("address3");
+
+    if(nameInput.value.trim().length || address1.value.trim().length || address2.trim().length || address3.trim().length !== 0){
+        nameInput.value = ("");
+        address1.value =("");
+        address2.value =("");
+        address3.value =("");
+    }
+});
+
