076ef78e5470ee57d0ca076d3f9c235605e55370
MOMO <rhwoah7190@gmail.com>
2023-12-20 17:47:46 +0900

    쇼핑몰 헤더 + 스케쥴러

---
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/scheduling/service/SchedulingService.java
@@ -0,0 +1,9 @@
+package com.TeamFiestar.Fiestar.scheduling.service;
+
+import java.util.Map;
+
+public interface SchedulingService {
+
+	Map<String, Object> selectDbImageList();
+
+}
---
+++ Fiestar/src/main/java/com/TeamFiestar/Fiestar/scheduling/service/SchedulingServiceImpl.java
@@ -0,0 +1,22 @@
+package com.TeamFiestar.Fiestar.scheduling.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.TeamFiestar.Fiestar.scheduling.mapper.SchedulingMapper;
+
+
+@Service
+public class SchedulingServiceImpl implements SchedulingService{
+
+	@Autowired
+	private SchedulingMapper mapper;
+	
+	@Override
+	public Map<String, Object> selectDbImageList() {
+		List<String> artistList = mapper.artistList();
+	}
+}
